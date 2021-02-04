9b5f478398ae62e90bf7a9beaee86990ce18bc2b
Matt Pfeiffer <mattpfeiffer54@gmail.com>
2021-02-04 07:21:59 -0600

    Initial Commit

---
+++ AudioRecipes/AudioRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecipesApp.swift
+//  AudioRecipes
+//
+//  Created by Matt Pfeiffer on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
